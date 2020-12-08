be3822b89f4f084864dd031fad3de620bb23249a
max maksimovic <maxmaksimovicROC@gmail.com>
2020-12-09 00:40:07 +0100

    awd

--- 5-Conditional_Statements/taak04- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/taak04- if, elseif, else conditonal/index.php
@@ -3,7 +3,16 @@
 <body>
 
 <?php
- 
+   $prod_besteld = 9;
+
+   if($prod_besteld < 10) {
+       $prijs = 1.50;
+   } elseif($prod_besteld < 20) {
+       $prijs = 1.25;
+   } else {
+       $prijs = 1.00;
+   }
+   echo $prijs;
 ?>
 
 </body>
--- 5-Conditional_Statements/taak06 - Logical Operators/index.php
+++ 5-Conditional_Statements/taak06 - Logical Operators/index.php
@@ -3,7 +3,37 @@
 <body>
 
 <?php
- 
+    $jan1 = 4;
+    $jan2 = 9;
+    if($jan1 < 10 && $jan1 > 2) {
+        echo "het getal zit tussen de 10 en 2";
+    } else {
+        echo "het getal zit niet tussen de 10 en 2";
+    }
+
+    echo "<br>";
+
+    if(!$jan2 = 5){
+        echo "Jan2 is 5";
+    } else {
+        echo "jan2 is niet 5"
+    }
+
+    echo "<br>";
+
+    if($jan1 == 4|| jan2 == 7){
+        echo "een van de jan is waar";
+    } else {
+        echo"geen jan is waar";
+    }
+
+    echo "<br>";
+
+    if($jan1 == 4 xor $jan2 == 8){
+        echo "een van de 2 is waar ";
+    } else {
+        echo "beide zijn false";
+    }
 ?>
 
 </body>
